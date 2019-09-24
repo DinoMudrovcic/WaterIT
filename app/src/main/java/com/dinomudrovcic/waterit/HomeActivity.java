499a9c2b1e1f65311b9afbcd23f3ced5b0d47e31
DinoMudrovcic <dinomudrovcic@gmail.com>
2019-09-25 01:26:48 +0200

    display name; exit application;

--- app/src/main/java/com/dinomudrovcic/waterit/HomeActivity.java
+++ app/src/main/java/com/dinomudrovcic/waterit/HomeActivity.java
@@ -2,18 +2,35 @@ package com.dinomudrovcic.waterit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
+import com.dinomudrovcic.waterit.models.User;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class HomeActivity extends Activity {
 
+    boolean doubleBackToExitPressedOnce = false;
+
     @BindView(R.id.tvKorisnik)
     TextView tvKorisnik;
+
+    DatabaseReference dbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +39,47 @@ public class HomeActivity extends Activity {
 
         ButterKnife.bind(this);
 
-        tvKorisnik.setText("Ulogirani korisnik je: " + FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        dbRef = FirebaseDatabase.getInstance().getReference();
+
+        String UID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("/users/" + UID);
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists()){
+
+                    User user = dataSnapshot.getValue(User.class);
+
+                    tvKorisnik.setText("Ulogirani korisnik je: " + user.display_name);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            FirebaseAuth.getInstance().signOut();
+            finishAffinity();
+            moveTaskToBack(true);
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
     }
 }
